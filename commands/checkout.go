de270f0976643ba9143f6366cb910d688c9fe6a8
Jonathan McCann <jonathan.mccann@liferay.com>
2017-02-01 12:34:58 -0800

    Format the branch name correctly

--- commands/checkout.go
+++ commands/checkout.go
@@ -88,7 +88,7 @@ func transformCheckoutArgs(args *Args) error {
 	user := headRepo.Owner.Login
 
 	if newBranchName == "" {
-		newBranchName = fmt.Sprintf("%s-%s", user, branch)
+		newBranchName = fmt.Sprintf("%s-%s-%s", "pull-request", id, branch)
 	}
 
 	repo, err := github.LocalRepo()
